fa1e3a3374deb43c7bc24bee191db92ca255089b
T-Aruga <baideneo@gmail.com>
2021-02-06 09:56:30 +0900

    Add tmp save logic

--- front/src/components/AddScheduleDialog/presentation.jsx
+++ front/src/components/AddScheduleDialog/presentation.jsx
@@ -30,8 +30,8 @@ const AddScheduleDialog = ({
     isDialogOpen
   },
   closeDialog,
-
-  setSchedule
+  setSchedule,
+  saveSchedule
 
   }) => {
   return (
@@ -88,7 +88,7 @@ const AddScheduleDialog = ({
         </Grid>
       </DialogContent>
       <DialogActions>
-        <Button color="primary" variant="outlined">
+        <Button color="primary" variant="outlined" onClick={saveSchedule}>
           保存
         </Button>
       </DialogActions>
